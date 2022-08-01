7eb9e1d470e7bfb4c44264d3964d14e08cd6e8d1
mfatima1010 <mehveenfatima1010@gmail.com>
2022-08-01 22:32:36 +0530

    day 1 added

---
+++ day_01.js
@@ -0,0 +1,49 @@
+// Multiple Arguements
+// console.log("Coding", "Is", "Fun", "!");
+
+//*****1*****
+// comments can make code readable
+
+//*****2*****
+//Welcome to 30DaysOfJavaScript
+
+//*****3*****
+/*comments can make code readable, 
+easy to reuse and informative 
+*/
+
+// *****4 and 5*****
+let var1 = "Mehveen";
+let var2 = true;
+let var3;
+let var4 = null;
+console.log(typeof var1);
+console.log(typeof var2);
+console.log(typeof var3);
+console.log(typeof var4);
+
+// *****6*****
+let a;
+let b;
+let c;
+let d;
+
+// *****7*****
+let name = "Mehveen";
+let hobby = "reading";
+const gender = "female";
+let college = "MJCET";
+
+// *****8 and 9*****
+let firstName = "Mehveen";
+let secondName = "Fatima";
+let maritalStatus = "unmarried";
+let country = "India";
+let age = 19;
+
+console.log(firstName, secondName, maritalStatus, country, age);
+console.log(firstName);
+console.log(secondName);
+console.log(maritalStatus);
+console.log(country);
+console.log(age);
